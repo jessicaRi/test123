241776ec6d7dc57e9c4d440c7da4aec5052000c7
jessicaRi <jessica.ri@hotmail.de>
2015-03-04 15:41:29 +0100

    Create movement.h

---
+++ movement.h
@@ -0,0 +1,16 @@
+/*
+ * movement.h
+ *
+ *  Created on: 04.03.2015
+ *      Author: Jessica
+ */
+
+#ifndef H_MOVEMENT_H_
+#define H_MOVEMENT_H_
+
+void rotate();
+void move();
+void stop();
+void search();
+
+#endif /* H_MOVEMENT_H_ */
