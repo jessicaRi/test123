f550dd9f801d92a1a73bc7aead8001ed34d85a95
jessicaRi <jessica.ri@hotmail.de>
2015-03-04 14:30:26 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "../h/main.h"
 #include "../h/movement.h"
+#include "../h/drueck.h"
 
 /// DO NOT DELETE THIS METHOD
 /// It is called every 1ms and e.g. can be used for implementing a
@@ -13,36 +14,52 @@ void ecrobot_device_initialize(void) {
 void ecrobot_device_terminate(void) {
 }
 
-
-
 void hello_world() {
 	ecrobot_status_monitor("Hello, World!");
 
 }
 
 TASK(OSEK_Main_Task) {
-	while (1) {
+	while(1) {
+		//Variablen
 		hello_world();
+		rotate();
 		int Helligkeit;
+		int Druck1;
+		int Druck2;
+
+		Druck1 = ecrobot_get_touch_sensor(NXT_PORT_S1);
+		Druck2 = ecrobot_get_touch_sensor(NXT_PORT_S4);
+
+		//Hinderniserkennung
+		if(Druck1 && Druck2 == 1){
+			drueck();
+		}
+
+
 		/* 500msec wait */
 		systick_wait_ms(500);
 
+		/*Helligkeit*/
+		ecrobot_set_light_sensor_active(NXT_PORT_S3);
+		Helligkeit = ecrobot_get_light_sensor(NXT_PORT_S3);
 
-		//nxt_motor_set_speed(NXT_PORT_B, 100, 0);
-		/* Helligkeit */
-		ecrobot_set_light_sensor_active(NXT_PORT_S3); //light on
-		Helligkeit = ecrobot_get_light_sensor(NXT_PORT_S3); //show value
-
-
-
+		/*Drive along line*/
+		if(Helligkeit > 500){
+			nxt_motor_set_speed(NXT_PORT_B,70,0);
+			nxt_motor_set_speed(NXT_PORT_C,70,0);
+		}
+		/*Stop if white*/
+		else {
+			nxt_motor_set_speed(NXT_PORT_B,60,1);
+			nxt_motor_set_speed(NXT_PORT_C,-60,1);
+		}
 
 		/* Drive along line if black */
 		if(Helligkeit > 500){
-			nxt_motor_set_speed(NXT_PORT_B, 70, 0);
-			nxt_motor_set_speed(NXT_PORT_C, 70, 0);
-		}/*Turn if white*/ else{
-			rotate();
-			}
+		nxt_motor_set_speed(NXT_PORT_B, 70, 0);
+		nxt_motor_set_speed(NXT_PORT_C, 70, 0);
 		}
-	}
 
+
+	}
